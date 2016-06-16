280e9d8efc14bf0af6326ac828dcbc251901b61f
Laxmik <laxmi.h818@gmail.com>
2016-06-16 14:12:49 -0400

    Predicts weather

---
+++ wethr.php
@@ -0,0 +1,12 @@
+<?php
+$city = $_GET['city'];
+
+$city = str_replace(" ", "" , $city);
+
+$contents=file_get_contents("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
+
+preg_match("'<span class=\"phrase\">(.*?)</span>'si", $contents,$matches);
+
+echo $matches[1];
+?>
+
